7e3ed56702f681d20bdf79ed323430476f92a1ad
mayjel <maijalesalva@gmail.com>
2024-09-28 11:14:52 +0800

    second task

---
+++ task2.py
@@ -0,0 +1,13 @@
+age =[-12, 13, -14, 15, -16]
+username =["juan", "pedro", "james", "natoy cute", "oscar"]
+
+for dataAge in age:
+    if dataAge < 0:
+        print(str(dataAge) + "negative")
+        elif dataAge > 0:
+            print(str(dataAge) + "positive")
+            
+for dataUser in username:
+    if dataUser =="natoy cute"
+    else:
+        print("natoy cute wala")
